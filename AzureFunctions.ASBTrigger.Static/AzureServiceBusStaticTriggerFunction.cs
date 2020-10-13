67e8dbc191bce8da0c007076f35a1445361e715c
Sean Feldman <feldman.sean@gmail.com>
2020-10-12 21:26:17 -0600

    Added a static version of NServiceBus FunctionEndpoint

---
+++ AzureFunctions.ASBTrigger.Static/AzureServiceBusStaticTriggerFunction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System.Threading.Tasks;
+using AzureFunctions.ASBTrigger.DI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class AzureServiceBusStaticTriggerFunction
+{
+    private const string EndpointName = "ASBTriggerQueue";
+
+    [FunctionName(EndpointName)]
+    public async Task Run(
+        [ServiceBusTrigger(queueName: EndpointName)]
+        Message message,
+        ILogger logger,
+        ExecutionContext executionContext)
+    {
+        await endpoint.Process(message, executionContext, logger);
+    }
+
+    private static readonly FunctionEndpoint endpoint = new FunctionEndpoint(executionContext =>
+    {
+        var configuration = ServiceBusTriggeredEndpointConfiguration.FromAttributes();
+
+        configuration.UseSerialization<NewtonsoftSerializer>();
+
+        configuration.LogDiagnostics();
+
+        var containerSettings = configuration.AdvancedConfiguration.UseContainer(new DefaultServiceProviderFactory());
+        var serviceCollection = containerSettings.ServiceCollection;
+
+        var configurationRoot = new ConfigurationBuilder()
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("local.settings.json")
+            .AddUserSecrets<AzureServiceBusStaticTriggerFunction>()
+            .AddEnvironmentVariables()
+            .Build();
+
+        serviceCollection.AddDbContext<MyDbContext>(delegate (DbContextOptionsBuilder options)
+        {
+            var connectionString = configurationRoot.GetConnectionString("MyDbConnectionString");
+            options.UseSqlServer(connectionString);
+        });
+
+        serviceCollection.AddScoped(typeof(MyService));
+        serviceCollection.AddScoped<IMyService>(provider => provider.GetRequiredService<MyService>());
+
+        return configuration;
+    });
+}
+
+public class MyService : IMyService
+{
+    public string SayHello() => "Hello!";
+}
+
+public interface IMyService
+{
+    string SayHello();
+}
---
+++ AzureFunctions.ASBTrigger.Static/TriggerMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AzureFunctions.ASBTrigger.DI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class TriggerMessageHandler : IHandleMessages<TriggerMessage>
+{
+    private static readonly ILog Log = LogManager.GetLogger<TriggerMessageHandler>();
+    private readonly IMyService myService;
+    private readonly MyDbContext myDbContext;
+
+    public TriggerMessageHandler(IMyService myService, MyDbContext myDbContext)
+    {
+        this.myService = myService;
+        this.myDbContext = myDbContext;
+    }
+
+    public async Task Handle(TriggerMessage message, IMessageHandlerContext context)
+    {
+        Log.Warn($"Handling {nameof(TriggerMessage)} in {nameof(TriggerMessageHandler)}. Service says: {myService.SayHello()}");
+
+        var any = await myDbContext.Users.AnyAsync();
+
+        Log.Info($"Found any users: {any}");
+    }
+}
